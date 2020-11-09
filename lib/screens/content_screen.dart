dd57bde6e07b50cf0ab70fb0b4437a75b72c7523
Vitor F Dullens <vitordullens2.0@gmail.com>
2020-11-09 02:24:28 -0300

    Changed to chewie, for video playback

--- lib/screens/content_screen.dart
+++ lib/screens/content_screen.dart
@@ -11,73 +11,87 @@ class ContentScreen extends StatefulWidget {
 
 class _ContentScreenState extends State<ContentScreen> {
   VideoPlayerController _controller;
-  Future<void> _initializeVideoPlayerFuture;
+  ChewieController _chewieController;
 
   @override
   void initState() {
     super.initState();
-    _controller = VideoPlayerController.asset(
-      "assets/videos/cap1.mp4",
+    this.initializePlayer();
+  }
+
+  Future<void> initializePlayer() async {
+    _controller = VideoPlayerController.asset('assets/videos/cap1.mp4');
+    await _controller.initialize();
+    _chewieController = ChewieController(
+      videoPlayerController: _controller,
+      looping: true,
+      aspectRatio: _controller.value.aspectRatio,
+      autoInitialize: true,
     );
-    _initializeVideoPlayerFuture = _controller.initialize();
+    setState(() {});
   }
 
   @override
   void dispose() {
     _controller.dispose();
+    _chewieController.dispose();
     super.dispose();
   }
 
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        resizeToAvoidBottomInset: true,
-        appBar: AppBar(
-          title: Text("Capítulo 1"),
-          backgroundColor: Color(0xFF6CA8F1),
-          leading: new IconButton(
-            icon: new Icon(Icons.arrow_back),
-            onPressed: () {
-              Navigator.pop(context, widget.content);
-            },
-          ),
-        ),
-        body: SingleChildScrollView(
-          child: Container(
-            padding: EdgeInsets.symmetric(
-              horizontal: 40.0,
-              vertical: 20.0,
-            ),
-            width: double.infinity,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                CircleAvatar(
-                    radius: 80,
-                    backgroundImage: AssetImage('assets/images/cap1.png')),
-                SizedBox(height: 30),
-                Center(
-                    child: AspectRatio(
-                  aspectRatio: _controller.value.aspectRatio,
-                  child: VideoPlayer(_controller),
-                )),
-                SizedBox(height: 30),
-                Text("${widget.content['conteudo']}"),
-              ],
-            ),
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          backgroundColor: Color(0xFF6CA8F1),
+      resizeToAvoidBottomInset: true,
+      appBar: AppBar(
+        title: Text("Capítulo 1"),
+        backgroundColor: Color(0xFF6CA8F1),
+        leading: new IconButton(
+          icon: new Icon(Icons.arrow_back),
           onPressed: () {
-            setState(() {
-              _controller.value.isPlaying
-                  ? _controller.pause()
-                  : _controller.play();
-            });
+            Navigator.pop(context, widget.content);
           },
-          child: Icon(
-            _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.symmetric(
+            horizontal: 40.0,
+            vertical: 20.0,
           ),
-        ));
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                  radius: 80,
+                  backgroundImage: AssetImage('assets/images/cap1.png')),
+              SizedBox(height: 30),
+              AspectRatio(
+                aspectRatio: _controller.value.aspectRatio,
+                child: Center(
+                  child: _chewieController != null &&
+                          _chewieController
+                              .videoPlayerController.value.initialized
+                      ? Chewie(
+                          controller: _chewieController,
+                        )
+                      : Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            CircularProgressIndicator(),
+                            SizedBox(height: 20),
+                            Text('Loading'),
+                          ],
+                        ),
+                ),
+              ),
+              SizedBox(height: 30),
+              Text("${widget.content['conteudo']}"),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
